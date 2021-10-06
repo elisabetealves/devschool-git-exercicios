edb82be63cf3714961d89a9e5a159523d9142350
Liz alves <elisabete_carente@hotmail.com>
2021-10-06 20:50:43 -0300

    add aluna elisabete

--- src/turma/turma.ts
+++ src/turma/turma.ts
@@ -1,5 +1,6 @@
 import { ALAN_JHONNES } from "../alunos/alan-jhonnes";
+import { ELISABETE_SANTOS } from "../alunos/elisabete-santos";
 
 export const turma: string[] = [
-    ALAN_JHONNES,
+    ALAN_JHONNES, ELISABETE_SANTOS
 ]
